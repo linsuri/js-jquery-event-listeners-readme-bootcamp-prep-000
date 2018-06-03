63b9672938bfb9cbe2d608a9d6f36d8143e47267
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-03 04:14:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
 
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  $('img').addClass("tasty");
+  $('img').css('border', "solid 2px red");
+}
+
+function pressIt(key) {
+  if (key.which == 71) {
+    alert('You have pressed G.');
+  }
+}
+
+function submitIt() {
+  alert('Your form is going to be submitted now.')
+}
+
 $(document).ready(function(){
 
 // call functions here
+
+$('p').on('click', getIt);
+$('img').on('load', frameIt);
+$('input').on('keydown', pressIt);
+$('form').on('submit', submitIt);
 
 });
